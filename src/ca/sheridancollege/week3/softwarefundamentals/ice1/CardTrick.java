d23ddb8fb1b40ea34a795877fd74880b7b0f9e81
Hon Huy Ngo <58998185+ngohonhuy@users.noreply.github.com>
2020-01-20 11:03:21 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,7 +23,11 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
-        
+         for (int i = 0; i<magicHand.length; i++)
+        {
+            System.out.println(magicHand[i].getSuit()+" "+magicHand[i].getValue());
+        }
+
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
