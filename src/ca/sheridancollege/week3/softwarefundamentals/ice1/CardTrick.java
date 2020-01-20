ab64c2ce3368d08ebf126a84662eb906671016ed
Yanjun Fang <fangyan@sheridancollege.ca>
2020-01-20 11:25:24 -0500

    created seven cards

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * modified by: Yanjun Fang(991562361)
  */
 public class CardTrick {
     
@@ -21,9 +22,13 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random()*13+1));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;  
+            System.out.print(magicHand[i]+" ");
         }
-        
+               
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
