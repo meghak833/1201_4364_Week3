ac190aa117c77838451c0cda82546153d73998c2
Varun <Varun@10.16.58.120>
2020-01-20 10:56:55 -0500

    created algorithm that chooses random numbers and suits for the card hand

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -13,6 +13,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author Megha Patel
+ * modified by Varun Vadali, 991580818
  */
 public class Card {
 
@@ -46,8 +47,5 @@ public class Card {
      */
     public void setValue(int value) {
         this.value = value;
-    }
-   
-   
-    
+    }   
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,14 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * modified by Varun Vadali, 991580818
  */
 public class CardTrick {
     
@@ -20,13 +23,16 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            Random rand = new Random();
+            c.setValue(rand.nextInt(13)+1);
             //c.setValue(insert call to random number generator here)
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            magicHand [i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
     }
-    
 }
