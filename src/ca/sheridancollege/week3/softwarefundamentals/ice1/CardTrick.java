2cb1b9299c2bb4ca306ad2ee034d4fbb6625a3c9
Jason <lee133@sheridancollege.ca>
2020-01-20 11:02:34 -0500

    Card trick class complete

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,14 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * Modified by Jason Lee 991476174
  */
 public class CardTrick {
     
@@ -20,8 +23,9 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random r = new Random();
+            c.setValue(1+r.nextInt(0-12));
+            c.setSuit(Card.SUITS[r.nextInt(0-3)]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
