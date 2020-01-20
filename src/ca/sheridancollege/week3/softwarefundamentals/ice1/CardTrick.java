131977ed5f91c87281b26889efb1edd89e508086
BrandonMartins <59835076+BrandonMartins@users.noreply.github.com>
2020-01-20 11:26:16 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,22 +11,38 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
  */
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author (Modified) Brandon Martins
+ * Student ID:991572898
+ */
 public class CardTrick {
-    
-    public static void main(String[] args)
+ public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
+        Scanner input = new Scanner(System.in);
+        String card[] = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+        String suit[] = {"Hearts", "Diamonds","Clubs","Spades"};
         
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
+        Card[] magicHand = new Card[7];
+
+     for (int i=0; i<magicHand.length; i++) {
+         Card c = new Card();
+         //c.setValue(insert call to random number generator here)
+         Random random = new Random(13);
+         //c.setSuit(Card.SUITS[insert call to random number between 0-3 here]
+         int Setvalue = random.nextInt(card.length * suit.length);
+     }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
+        System.out.println("Enter your lucky card!");
+        String luckyCard = input.next(); //Assume the user inputs correct information
+        
+        
         //Then report the result here
+        System.out.println("Your lucky card was " + luckyCard + ", and your hand is" + magicHand);
     }
-    
 }
