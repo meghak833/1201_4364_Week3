809070cc475485b12fa38283cd5d8888e8aeac3e
alkayemm <alkayemm@sheridancollege.ca>
2020-01-20 10:43:08 -0600

    Added rng

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,7 +9,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author Megha Patel
+ * @author Mohammed Al-Kayem
  */
 public class CardTrick {
     
@@ -17,11 +17,13 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        int x = (int)Math.random();
+        int y = (int)((Math.random() * (3 - 1) +1 ));
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(x);
+            //c.setSuit(Card.SUITS[y])
         }
         
         //insert code to ask the user for Card value and suit, create their card
