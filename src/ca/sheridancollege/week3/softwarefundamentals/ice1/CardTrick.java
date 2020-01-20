eac1fc645a06d2c42afc2eb41aa31a98998c1a88
bradmonster <bradmonster@10.211.55.7>
2020-01-20 10:58:53 -0500

    completed the hand for loop

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,15 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * Modified: Bradley Persaud
  */
 public class CardTrick {
     
@@ -17,11 +21,16 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        
+        
         for (int i=0; i<magicHand.length; i++)
         {
+            Random rand = new Random();
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            c.setValue(rand.nextInt(13)+1);
+            c.setSuit(Card.SUITS[rand.nextInt(3)]);
+            magicHand [i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
